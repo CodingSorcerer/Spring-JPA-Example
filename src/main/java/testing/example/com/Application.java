84a3a2e871275aef3a525b3efc0102a42f916bf8
Rick Marczak <rick.marczak@accenture.com>
2019-02-11 08:35:45 -0500

    updated to use generic crud controller

--- src/main/java/testing/example/com/Application.java
+++ src/main/java/testing/example/com/Application.java
@@ -2,8 +2,10 @@ package testing.example.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "net.lonewolfcode.opensource.springutilities")
 public class Application
 {
     public static void main(String[] Args)
--- src/main/java/testing/example/com/repositories/EmployeeRepository.java
+++ src/main/java/testing/example/com/repositories/EmployeeRepository.java
@@ -1,10 +1,12 @@
 package testing.example.com.repositories;
 
+import net.lonewolfcode.opensource.springutilities.annotations.CrudRepo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import testing.example.com.dataObjects.Employee;
 
 @Repository
+@CrudRepo(entityClass = Employee.class, basePathName = "test")
 public interface EmployeeRepository extends CrudRepository<Employee,String>
 {
 }
